47057094ae52a9efb00c8c82eed2ff1d15513377
Priamo Meloni <priamomeloni@hotmail.it>
2017-04-07 08:04:30 +0000

    Update StupidTest.php

--- test/StupidTest.php
+++ test/StupidTest.php
@@ -7,5 +7,12 @@ class StupidTest extends \PHPUnit_Framework_TestCase
         $foo = true;
         $this->assertTrue($foo);
     }
+     public function testHTML()
+     {
+         $html = '<p id="my_id">ciao,Oao</p>';
+         $matcher = array('id'=> 'my_id');
+         $this->assertTag($matcher, $html);
+     }
 }
+
 
